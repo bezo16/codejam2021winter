c63495b95a08ce4c28f4b7dcc07726decdc395c6
bezo16 <bezo16@protonmail.ch>
2021-12-22 23:26:12 +0100

    .

--- components/TheGarden.vue
+++ components/TheGarden.vue
@@ -1,14 +1,17 @@
 <template>
   <section class="garden">
+      <img v-for="(santa,index) in elf" :key="index" class="entity" :style="`left:${elfPositions[index].left}%;top:${elfPositions[index].top}%;`" src="~/assets/img/entities/elf1.png" alt="">
       <p>made by deers: {{ reindeerProduced }}  made by elf: {{ elfProduced }} made by santas: {{ santaProduced }}</p>
-      <h1>{{reindeer}}</h1>
-      <h1>{{elf}}</h1>
-      <h1>{{santa}}</h1>
   </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      interval: null,
+    }
+  },
   computed: {
     reindeer() {
       return this.$store.state.localStorage.reindeer
@@ -16,11 +19,20 @@ export default {
     reindeerProduced() {
       return this.$store.state.localStorage.reindeerProduced
     },
+    reindeerChance() {
+      return this.$store.state.localStorage.reindeerChance
+    },
     elf() {
       return this.$store.state.localStorage.elf
     },
     elfProduced() {
       return this.$store.state.localStorage.elfProduced
+    },
+    elfChance() {
+      return this.$store.state.localStorage.elfChance
+    },
+    elfPositions() {
+      return this.$store.state.localStorage.elfPositions
     },
     santa() {
       return this.$store.state.localStorage.santa
@@ -28,25 +40,43 @@ export default {
     santaProduced() {
       return this.$store.state.localStorage.santaProduced
     },
+    santaChance() {
+      return this.$store.state.localStorage.santaChance
+    },
+  },
+  methods: {
+    randomNum() {
+      return Math.ceil(Math.random() * 90) + 5
+    }
   },
   mounted() {
-    setInterval(() => {
+
+      setInterval(() => {
       let reindeerLsd = 0
       for(let i=0; i < this.reindeer; i++ ) {
         let ranNum = Math.ceil(Math.random() * 100)
-        if(ranNum <= 5) reindeerLsd++
+        if(ranNum <= this.reindeerChance) reindeerLsd++
       }
       this.$store.commit('localStorage/addLsd',reindeerLsd)
       this.$store.commit('localStorage/updateProduction',{name:'reindeerProduced',value:reindeerLsd})
 
       let elfLsd = 0
-      for(let i=0; i < this.reindeer; i++ ) {
+      for(let i = 0; i < this.elf; i++ ) {
         let ranNum = Math.ceil(Math.random() * 100)
-        if(ranNum <= 15) elfLsd++
+        if(ranNum <= this.elfChance) elfLsd++
       }
       this.$store.commit('localStorage/addLsd',elfLsd)
+      this.$store.commit('localStorage/updateProduction',{name:'elfProduced',value:elfLsd})
 
-      this.$store.commit('localStorage/addLsd',this.santa)
+
+      let santaLsd = 0
+      for(let i = 0; i < this.santa; i++ ) {
+        let ranNum = Math.ceil(Math.random() * 100)
+        if(ranNum <= this.santaChance) santaLsd++
+      }
+      this.$store.commit('localStorage/addLsd',santaLsd)
+      this.$store.commit('localStorage/updateProduction',{name:'santaProduced',value:santaLsd})
+
     }, 1000);
   }
 }
@@ -56,5 +86,11 @@ export default {
   .garden {
     flex: 1;
     height: 100%;
+    position: relative;
+  }
+
+  .entity {
+    position: absolute;
+    width: 40px;
   }
 </style>
