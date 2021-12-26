8f8729fae854e538ca43b3f2cd430ee405b11442
bezo16 <bezo16@protonmail.ch>
2021-12-26 11:55:18 +0100

    .

--- pages/end.vue
+++ pages/end.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="end">
-    <h1>WOW u have won</h1>
+    <h1>You saved christmas!</h1>
+    <p>now can santa make some magic with lsd and make reindeers fly</p>
+    <img src="~/assets/img/santagif.gif" alt="">
   </section>
 </template>
 
@@ -11,5 +13,23 @@ export default {
 </script>
 
 <style>
+  .end {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #F1FFED;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='250' height='30' viewBox='0 0 1000 120'%3E%3Cg fill='none' stroke='%23B7B2CE' stroke-width='10' stroke-opacity='0.07'%3E%3Cpath d='M-500 75c0 0 125-30 250-30S0 75 0 75s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 45c0 0 125-30 250-30S0 45 0 45s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 105c0 0 125-30 250-30S0 105 0 105s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 15c0 0 125-30 250-30S0 15 0 15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500-15c0 0 125-30 250-30S0-15 0-15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 135c0 0 125-30 250-30S0 135 0 135s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3C/g%3E%3C/svg%3E");
+  }
 
+  .end > h1 {
+    font-size: 4rem;
+    margin-bottom: 50px;
+  }
+
+  .end > p {
+    font-size: 2rem;
+    margin-bottom: 70px;
+  }
 </style>
